ed3c6d89047a3d6a7bf9f9de5309f75c178b9c5c
AfifiMac <ahmedabdallah3996@gmail.com>
2026-06-22 05:21:19 +0300

    Untrack app/build folder and ignore build/ and .kotlin/ directories

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_tviptv_app_TvIptvApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.tviptv.app.TvIptvApplication"
-)
-public final class _com_tviptv_app_TvIptvApplication {
-}
